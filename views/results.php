503e5f16565417598857700101e583f34688658c
alexandru <alexandru_visan@telekom.ro>
2017-08-09 13:50:55 +0300

    fine tunnig

--- views/results.php
+++ views/results.php
@@ -12,18 +12,18 @@ if(!defined('denyAccess')) { die('Direct access not permitted');}
 			<li class="active">
         <a href="#list" data-toggle="tab">List</a>
 			</li>
-			<li><a href="#map" data-toggle="tab">Map</a>
+			<li>
+				<a href="#map" data-toggle="tab">Map</a>
 			</li>
 		</ul>
 
 		<div class="tab-content">
-		  <div class="tab-pane active" id="list">
-			</div>
-			<div class="tab-pane" id="map" style="position:absolute; width: 97%; height: 400px;">
-			</div>
+		  <div class="tab-pane active" id="list"></div>
+			<div class="tab-pane" id="map" style="position:absolute; width: 97%; height: 400px;"></div>
 		</div>
 
   </div>
 
   <ul id="pagination" class="pagination"></ul>
+
 </div>
